dba268aa10a7af4139af60e336b869e90e32f556
Invincible-warrior <157756234+Invincible-warrior@users.noreply.github.com>
2024-04-19 03:31:16 -0400

    Add files via upload

---
+++ public/assets/js/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjPBi88kfiaM5beEihm53LJpS59F-j_CU",
+    authDomain: "movie-app-5900f.firebaseapp.com",
+    projectId: "movie-app-5900f",
+    storageBucket: "movie-app-5900f.appspot.com",
+    messagingSenderId: "539040805905",
+    appId: "1:539040805905:web:f0c66dcd7166c586072db0",
+    measurementId: "G-G4HLL7B23W"
+  };
+  export default firebaseConfig;
